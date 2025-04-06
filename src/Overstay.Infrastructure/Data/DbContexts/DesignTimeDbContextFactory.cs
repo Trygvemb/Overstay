c305b41cd580b345e1a5165ac3be0cb92efdab14
trygvebechsgaard <trygvemb@gmail.com>
2025-04-06 14:54:37 +0700

    feat: pass seed data options to ApplicationDbContext feat: update launch settings with default launch URL refactor: fix migrations assembly path in DbContext registration

--- src/Overstay.Infrastructure/Data/DbContexts/DesignTimeDbContextFactory.cs
+++ src/Overstay.Infrastructure/Data/DbContexts/DesignTimeDbContextFactory.cs
@@ -33,6 +33,6 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<Applicatio
         configuration.GetSection("SeedData").Bind(seedConfigurations);
         var seedOptions = Options.Create(seedConfigurations);
 
-        return new ApplicationDbContext(optionsBuilder.Options, null!);
+        return new ApplicationDbContext(optionsBuilder.Options, seedOptions);
     }
 }
