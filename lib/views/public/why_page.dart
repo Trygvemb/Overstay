dcde2a3f09ba587731d3cbc08ad58e13d4aa51fa
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-10 14:37:22 +0200

    structurel changes

---
+++ lib/views/public/why_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WhyPage extends StatelessWidget {
+  const WhyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Why us page'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the about us page. Here you can find information on why you should use our app.',
+          style: TextStyle(fontSize: 20, color: Colors.black54),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
