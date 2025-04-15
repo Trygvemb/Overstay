aeb7fcacd0fac26c8f84715d4d77054a2a0afe6b
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-15 13:58:04 +0200

    updat

--- lib/views/app/widget_tree.dart
+++ lib/views/app/widget_tree.dart
@@ -2,6 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:overstay_frontend/data/notifiers.dart';
 import 'package:overstay_frontend/views/app/home_page.dart';
+import 'package:overstay_frontend/views/app/visa_page.dart';
+import 'package:overstay_frontend/views/app/profile_page.dart';
+import 'package:overstay_frontend/views/app/notifications_page.dart';
+import 'package:overstay_frontend/views/app/immigrationoffice_page.dart';
+import 'package:overstay_frontend/views/public/landing_page.dart';
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -10,16 +15,10 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Widget> pages = [
       const HomePage(), // index 0
-      const Placeholder(
-        // index 1 (fx VisaPage)
-        color: Colors.orange,
-        child: Center(child: Text('Visa Page')),
-      ),
-      const Placeholder(
-        // index 2 (fx ProfilePage)
-        color: Colors.green,
-        child: Center(child: Text('Profile Page')),
-      ),
+      const VisaPage(), // index 1
+      const ProfilePage(), // index 2
+      const NotificationsPage(), // index 3
+      const ImmigrationofficePage(), // index 4
     ];
 
     return ValueListenableBuilder<int>(
@@ -27,11 +26,28 @@ class WidgetTree extends StatelessWidget {
       builder: (context, selectedPage, child) {
         return Scaffold(
           appBar: AppBar(
+            // Top AppBar
+            backgroundColor: const Color(0xFF1A759F),
             title: const Text('Overstay - Inner Pages'),
-            backgroundColor: Colors.teal,
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (_) => const LandingPage()),
+                    (route) => false, // sikre at man har tømt stacken
+                  );
+                },
+                child: const Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
           body: pages[selectedPage],
           bottomNavigationBar: NavigationBar(
+            backgroundColor: const Color(0xFF1A759F),
             selectedIndex: selectedPage,
             onDestinationSelected: (newIndex) {
               selectedPageNotifier.value = newIndex;
@@ -40,6 +56,14 @@ class WidgetTree extends StatelessWidget {
               NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
               NavigationDestination(icon: Icon(Icons.flight), label: 'Visa'),
               NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+              NavigationDestination(
+                icon: Icon(Icons.notifications),
+                label: 'Notifications',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.location_pin),
+                label: 'Immigration offices',
+              ),
             ],
           ),
         );
