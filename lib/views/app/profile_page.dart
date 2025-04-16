4e8faeef0475c12d79397821023e2c6ef5fe441b
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-16 09:48:32 +0200

    profilr page update

--- lib/views/app/profile_page.dart
+++ lib/views/app/profile_page.dart
@@ -7,13 +7,19 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
+//controller til at håndtere tilstand og interaktioner i profilformularen
 class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: [
-          // venstre side
+          // venstre side gradient
           Expanded(
             flex: 1,
             child: Container(
@@ -51,31 +57,110 @@ class _ProfilePageState extends State<ProfilePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text(
-            'Profile',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Name',
-              border: OutlineInputBorder(),
+            'My Profile',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
-          const SizedBox(height: 16),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(),
-            ),
+          const SizedBox(height: 24),
+
+          //First name
+          Text(
+            'First Name',
+            style: TextStyle(fontSize: 16, color: Colors.black),
           ),
+          const SizedBox(height: 8),
+          _buildTextField(firstNameController, 'First name'),
           const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              // Handle save action
-            },
-            child: const Text('Save'),
+
+          //Last name
+          const Text(
+            'Last name',
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          const SizedBox(height: 8),
+          _buildTextField(lastNameController, 'Last name'),
+          const SizedBox(height: 16),
+
+          // Email
+          const Text(
+            'Email',
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          const SizedBox(height: 8),
+          _buildTextField(emailController, 'Email'),
+          const SizedBox(height: 16),
+
+          // Password
+          const Text(
+            'Password',
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          const SizedBox(height: 8),
+          _buildTextField(passwordController, 'Password', isPassword: true),
+          const SizedBox(height: 24),
+
+          //vandret streg
+          Divider(thickness: 2, color: const Color(0xFF1A759F)),
+          const SizedBox(height: 16),
+
+          // Row save og slet button
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  //handle save action
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF1A759F),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12,
+                  ),
+                ),
+                child: const Text(
+                  'save',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(width: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  //handle delete action
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF1A759F),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12,
+                  ),
+                ),
+                child: const Text(
+                  'Delete',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+    TextEditingController controller,
+    String hintText, {
+    bool isPassword = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
   }
