c9e89121cb4fff9aca657bb720789cad8885c43e
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-24 09:42:01 +0200

    created new files based on payload

---
+++ lib/models/sign_in_user_request.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+class SignInUserRequest {
+  final String userName;
+  final String password;
+
+  SignInUserRequest({required this.userName, required this.password});
+
+  Map<String, dynamic> toJson() => {'userName': userName, 'password': password};
+}
