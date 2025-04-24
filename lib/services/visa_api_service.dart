f4e04711b79c43ff87c3350db7fafda37f4aab85
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-24 10:24:15 +0200

    added files to /service

---
+++ lib/services/visa_api_service.dart
@@ -0,0 +1,39 @@
+import '../models/create_visa_request.dart';
+import '../models/update_visa_request.dart';
+import '../models/visa_respons.dart';
+import '../models/visa_type_response.dart';
+import 'api_service.dart';
+
+class VisaApiService extends ApiService {
+  /// GET /api/VisaType  – hent alle typer (til dropdown)
+  Future<List<VisaTypeResponse>> getVisaTypes() async {
+    final res = await get('/api/VisaType');
+    return parse(res, (j) {
+      final list = j as List<dynamic>;
+      return list
+          .map((e) => VisaTypeResponse.fromJson(e as Map<String, dynamic>))
+          .toList();
+    });
+  }
+
+  /// POST /api/Visa  – opret visa
+  Future<VisaResponse> createVisa(CreateVisaRequest req) async {
+    final res = await post('/api/Visa', req.toJson());
+    return parse(res, (j) => VisaResponse.fromJson(j));
+  }
+
+  /// PUT /api/Visa/{id}  – opdater visa
+  Future<void> updateVisa(String id, UpdateVisaRequest req) async {
+    final res = await put('/api/Visa/$id', req.toJson());
+    if (res.statusCode != 204) _throw(res);
+  }
+
+  /// DELETE /api/Visa/{id}
+  Future<void> deleteVisa(String id) async {
+    final res = await delete('/api/Visa/$id');
+    if (res.statusCode != 204) _throw(res);
+  }
+
+  void _throw(res) =>
+      throw Exception('API-fejl ${res.statusCode}: ${res.body}');
+}
