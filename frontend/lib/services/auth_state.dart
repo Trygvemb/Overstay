88d4d38297382f7fa7a55f2a6499bdefcc4028f6
LHjortlund <lbfe44184@edu.ucl.dk>
2025-05-06 08:49:25 +0200

    change to to remember username in auth_state

--- frontend/lib/services/auth_state.dart
+++ frontend/lib/services/auth_state.dart
@@ -4,15 +4,28 @@ class AuthState extends ChangeNotifier {
   String? jwt;
   bool isAdmin = false;
 
-  void setAuth(String token, bool admin) {
+  String? userName;
+  String? email;
+
+  //kaldes efter login
+  void setAuth({
+    required String token,
+    required bool admin,
+    required String userName,
+    required String email,
+  }) {
     jwt = token;
     isAdmin = admin;
+    this.userName = userName;
+    this.email = email;
     notifyListeners();
   }
 
   void clear() {
     jwt = null;
     isAdmin = false;
+    userName = null;
+    email = null;
     notifyListeners();
   }
 }
