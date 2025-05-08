e8d43f38e8ff55acce00ae6aec2c45c91d126ac1
trygvebechsgaard <trygvemb@gmail.com>
2025-05-08 08:48:28 +0700

    Update VisaTests to reflect IsActive property behavior

--- api/test/Overstay.UnitTest/Domain/Entities/VisaTests.cs
+++ api/test/Overstay.UnitTest/Domain/Entities/VisaTests.cs
@@ -6,23 +6,34 @@ namespace Overstay.UnitTest.Domain.Entities;
 public class VisaTests
 {
     [Fact]
-    public void IsExpired_ShouldReturnTrue_WhenExpired()
+    public void IsActive_ShouldBeFalse_WhenExpired()
     {
         var arrivalDateTime = DateTime.UtcNow;
         var expiredDateTime = arrivalDateTime - TimeSpan.FromHours(1);
         var visa = new Visa(arrivalDateTime, expiredDateTime);
 
-        visa.SetIsActive().ShouldBe(true);
+        visa.IsActive.ShouldBe(false);
     }
 
     [Fact]
-    public void IsExpired_ShouldReturnFalse_WhenNotExpired()
+    public void IsActive_ShouldBeTrue_WhenNotExpired()
     {
         var arrivalDateTime = DateTime.UtcNow;
         var expiredDateTime = arrivalDateTime + TimeSpan.FromHours(1);
         var visa = new Visa(arrivalDateTime, expiredDateTime);
 
-        visa.SetIsActive().ShouldBe(false);
+        visa.IsActive.ShouldBe(true);
+    }
+
+    [Fact]
+    public void SetIsActive_ShouldReturnFalse_WhenCalledWithFalse()
+    {
+        var arrivalDateTime = DateTime.UtcNow;
+        var expiredDateTime = arrivalDateTime + TimeSpan.FromHours(1);
+        var visa = new Visa(arrivalDateTime, expiredDateTime);
+
+        visa.SetIsActive(false).ShouldBe(false);
+        visa.IsActive.ShouldBe(false);
     }
 
     [Fact]
