ee5cff5df94b037d0f57054a374fae42cd1a5ba4
trygvebechsgaard <trygvemb@gmail.com>
2025-05-12 13:33:05 +0700

    Enhance DateToDateTimeConverter to support additional date formats and use ISO 8601 for output

--- api/src/Overstay.Application/Commons/Behaviors/DateToDateTimeConverter.cs
+++ api/src/Overstay.Application/Commons/Behaviors/DateToDateTimeConverter.cs
@@ -6,20 +6,41 @@ namespace Overstay.Application.Commons.Behaviors;
 
 public class DateToDateTimeConverter : JsonConverter<DateTime>
 {
-    private readonly string[] _formats = ["yyyy-MM-dd", "MM/dd/yyyy", "dd-MM-yyyy"];
+    private readonly string[] _formats =
+    [
+        "yyyy-MM-dd",
+        "MM/dd/yyyy",
+        "dd-MM-yyyy",
+        "o", // ISO 8601 format
+        "yyyy-MM-ddTHH:mm:ssZ", // Added format for datetime picker
+    ];
 
-    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override DateTime Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         var dateString = reader.GetString();
-        if (DateTime.TryParseExact(dateString, _formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+        if (
+            DateTime.TryParseExact(
+                dateString,
+                _formats,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.AdjustToUniversal,
+                out var date
+            )
+        )
         {
             return date;
         }
-        throw new JsonException($"Invalid date format. Supported formats: {string.Join(", ", _formats)}");
+        throw new JsonException(
+            $"Invalid date format. Supported formats: {string.Join(", ", _formats)}"
+        );
     }
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
     {
-        writer.WriteStringValue(value.ToString("yyyy-MM-dd"));
+        writer.WriteStringValue(value.ToString("o")); // Use ISO 8601 format for output
     }
 }
