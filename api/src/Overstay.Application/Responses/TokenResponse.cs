c1de58cccbc2a089b8d1463849237949e13b8b7c
Trygve Bechsgaard <trygvemb@gmail.com>
2025-05-14 08:57:34 +0700

    added UserId to tokenresponse (#11)

--- api/src/Overstay.Application/Responses/TokenResponse.cs
+++ api/src/Overstay.Application/Responses/TokenResponse.cs
@@ -3,6 +3,6 @@ namespace Overstay.Application.Responses;
 public class TokenResponse
 {
     public string AccessToken { get; set; } = string.Empty;
-    public string RefreshToken { get; set; } = string.Empty;
+    public Guid? UserId { get; set; }
     public DateTime ExpiresAt { get; set; }
 }
